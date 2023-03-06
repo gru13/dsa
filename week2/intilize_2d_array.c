eefb7dd56b6989171040e9bac188a14e718d576f
Guruprasath <44829747+gru13@users.noreply.github.com>
2023-03-06 05:12:42 +0000

    .
    Signed-off-by: Guruprasath <44829747+gru13@users.noreply.github.com>

---
+++ week2/intilize_2d_array.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int i , j;
+    puts("Pls enter no of row : ");
+    scanf("%d",&i);
+    puts("Pls enter no of col : ");
+    scanf("%d",&j);
+    return 0;
+}
