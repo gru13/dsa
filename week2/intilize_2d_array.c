654cecb987ee91ad860dc28e3282897b6dcbfd63
Guruprasath <44829747+gru13@users.noreply.github.com>
2023-03-06 05:20:49 +0000

    .
    Signed-off-by: Guruprasath <44829747+gru13@users.noreply.github.com>

--- week2/intilize_2d_array.c
+++ week2/intilize_2d_array.c
@@ -6,5 +6,20 @@ int main(){
     scanf("%d",&i);
     printf("Pls enter no of col : ");
     scanf("%d",&j);
+    
+    int arr[i][j];
+
+    for(int a = 0; a<i; a++){
+        for(int b = 0; b<j; b++){
+        printf("Ënter value of arr[%d][%d]", i,j);
+        scanf("%d", &arr[a][b]);
+        }
+    }
+
+    for(int a = 0; a<i; a++){
+        for(int b = 0; b<j; b++){
+        printf("Ënter value of %d\n", arr[a][b]);
+        }
+    }
     return 0;
 }
