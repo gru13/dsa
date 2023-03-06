c0dc0a8cd5c572150d0091c06ffd761e8c758b44
Guruprasath <44829747+gru13@users.noreply.github.com>
2023-03-06 05:34:22 +0000

    .
    Signed-off-by: Guruprasath <44829747+gru13@users.noreply.github.com>

---
+++ week2/insert_element_in_array.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main(){
+    int i , j;
+    printf("Pls enter no of row : ");
+    scanf("%d",&i);
+    printf("Pls enter no of col : ");
+    scanf("%d",&j);
+    
+    int arr[i][j];
+
+    for(int a = 0; a<i; a++){
+        for(int b = 0; b<j; b++){
+        printf("Ënter value of arr[%d][%d]", a,b);
+        scanf("%d", &arr[a][b]);
+        }
+    }
+    // asking which index to change element to change 
+    int index1, index2, value;
+    printf("Ënter indexs : ");
+    scanf("%d %d",index1, index2);
+
+    // asking which value to change element to change 
+    int index1, index2;
+    printf("Ënter value : ");
+    scanf("%d",value);
+
+    for(int a = 0; a<i; a++){
+        for(int b = 0; b<j; b++){
+        printf("Ënter value of %d\n", arr[a][b]);
+        }
+    }
+    return 0;
+}
