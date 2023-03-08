0068c7828c35a3065e7f8dae9bd656cc613d50cd
gru13 <guruprasath1302@gmail.com>
2023-03-08 16:34:08 +0530

    ll insert at middle
    Signed-off-by: gru13 <guruprasath1302@gmail.com>

--- linked_list.c
+++ linked_list.c
@@ -14,6 +14,45 @@ void display(struct Node *head){
     }
 }
 
+int len(struct Node* head){
+    struct Node *rmp = head;
+    int length = 0;
+    while(rmp != NULL){
+        length++;
+        rmp = rmp->next;
+    }
+    return length;
+}
+void insert(int index,int value,struct Node *head){
+    int length = len(head);
+    struct Node *tmp = head;
+    struct Node *new = (struct Node*)malloc(sizeof(struct Node*));
+    new->value = value;
+    if (index > length || index < 0){printf("can't inserted due to lenght");}
+    else{
+        if(index == 0){
+            new->next = tmp;
+            head = new;
+            display(head);
+        }
+        else if(index == length){
+            while(tmp->next != NULL){
+                tmp = tmp->next;
+            }
+            tmp->next = new;
+            new->next = NULL;
+        }
+        else{
+            for(int i = 0;i<index-1;i++){
+                tmp = tmp->next;
+            }
+            printf("%d\n",tmp->value);
+            new->next = tmp->next;
+            tmp->next = new; 
+            // display(head);
+        }
+    }
+}
 int main(){
     struct Node *head;
     
@@ -45,5 +84,7 @@ int main(){
     third->value = 3;
     third->next = NULL;  
     
+    insert(len(head),4,head);
+    printf("\t%d\n",len(head));
     display(head);
 }
