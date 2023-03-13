e851c160258664cfbf58e7c68d7a362e54d91934
Guruprasath <44829747+gru13@users.noreply.github.com>
2023-03-13 04:23:44 +0000

    week 3 display,len
    Signed-off-by: Guruprasath <44829747+gru13@users.noreply.github.com>

--- week3/linkedlist.c
+++ week3/linkedlist.c
@@ -7,8 +7,47 @@ struct Node {
     
 };
 
+int len(struct Node* head){
+    struct Node* tmp = head;
+    int len = 0; 
+    while (tmp != NULL)
+    {
+        len++;
+        tmp = tmp->next;
+    }
+    return len;
+}
+
+
+void display(struct Node* head){
+    struct Node* tmp = head;
+    while (tmp != NULL)
+    {
+        printf("%d -> ",tmp->data);
+        tmp = tmp->next;
+    }
+    printf("NULL\n");
+}
 
 int main(){
-    
+    struct Node* first = (struct Node*)malloc(sizeof(struct Node*));
+    struct Node* second = (struct Node*)malloc(sizeof(struct Node*));
+    struct Node* third = (struct Node*)malloc(sizeof(struct Node*));
+    struct Node* four = (struct Node*)malloc(sizeof(struct Node*));
+
+    first->data = 1;
+    second->data = 2;
+    third->data = 3;
+    four->data = 4;
+
+    first->next = second;
+    second->next = third;
+    third->next = four;
+    four->next = NULL;
+
+    struct Node* head = first;
+
+    display(head);
+    printf("%d\n",len(head));
     return 0;
 }
