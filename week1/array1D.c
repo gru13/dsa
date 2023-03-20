7be716249d6c56967e41745eeedd50101a55e173
gru13 <guruprasath1302@gmail.com>
2023-03-20 19:26:54 +0530

    .\
    Signed-off-by: gru13 <guruprasath1302@gmail.com>

--- week1/array1D.c
+++ week1/array1D.c
@@ -59,7 +59,7 @@ int* delete(int *arr,int len,int index){
 int* sort(int* arr, int len){
     
     for(int i = 0; i<len; i++){
-	   for(int j = 0; j<len-i; j++){
+	   for(int j = 0; j<len-i-1; j++){
 		 if(*(arr+j)>*(arr+j+1)){
 			 swap((arr+j),(arr+j+1));
 		 }	
