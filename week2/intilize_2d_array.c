03d2f7a9fd6e1584996f04a476ef447adbf68c3e
gru13 <guruprasath1302@gmail.com>
2023-03-26 17:33:00 +0530

    row sort
    Signed-off-by: gru13 <guruprasath1302@gmail.com>

--- week2/intilize_2d_array.c
+++ week2/intilize_2d_array.c
@@ -24,6 +24,7 @@ int **create_empty(int i, int j) {
 
 
 void display(int** arr){
+    printf("\n");
     for(int i =0; i<ROW;i++){
         for(int j = 0;j<COL;j++){
             printf("%d\t",arr[i][j]);
@@ -58,19 +59,50 @@ int** delete_row(int **arr,int row){
     return arr1;
 }
 
-int** insert_row(int **arr,int row){
-    int** arr1 = create_empty(ROW+1,COL);
-    for(int i = 0;i>ROW;i++){
-        if(i<row){
-            arr1[i] = arr1[i]; 
+int** insert_row(int **arr,int row_del){
+    int ** arr1 = create_empty(ROW-1,COL);
+    for(int i = 0;i<ROW+1;i++){
+        if(i<row_del){
+            arr1[i] = arr[i];
         }
-        else if
+        else if(i == row_del){
+            for(int j = 0;j<COL;j++){
+                printf("Enter the valuee for index arr[%d][%d] : ",i,j);
+                scanf("%d",&arr1[i][j]);
+            }
+        }
+        else{
+            arr1[i] = arr[i-1];
+        }
     }
     ROW++;
     free(arr);
     return arr1;
 }
 
+int sum(int ** arr){
+    int sum =0;
+    for(int i =0;i<ROW;i++){
+        for(int j=0;j<COL;j++){
+            sum += arr[i][j];
+        }
+    }
+    return sum;
+}
+
+
+int** sort_row(int** arr){
+    for(int i = 0;i<ROW;i++){
+        for(int j = 0 ;j<ROW-i-1;j++){
+            if(arr[j][0] > arr[j+1][0]){
+                int *temp = arr[j];
+                arr[j] = arr[j+1]; 
+                arr[j+1] = temp; 
+            }
+        }
+    }
+    return arr;
+}
 
 int main(){
     int i , j;
@@ -85,7 +117,10 @@ int main(){
     // arr = delete(arr,1,1);
     // display(arr);
     // arr = update(arr,1,1,999);
-    arr = delete_row(arr,1);
+    // arr = delete_row(arr,1);
+    // arr = insert_row(arr,1);
+    // printf("%d",sum(arr));
+    arr = sort_row(arr);
     display(arr);
 
 
