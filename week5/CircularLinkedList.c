2f15d045ff2eb2553578898409c9a7cc7ed3f43d
Guruprasath <guruprasath1302@gmail.com>
2023-03-27 06:26:01 +0000

    cll started

--- week5/CircularLinkedList.c
+++ week5/CircularLinkedList.c
@@ -5,34 +5,56 @@ struct Node{
     int data;
     struct Node* next;
 };
+void display(struct Node* last){
+    struct Node* tmp = last->next;
+    printf("||->");
+    while(tmp != last){
+        printf(" %d ->",tmp->data);
+        tmp = tmp->next;
+    }
+    printf("||");
+}
 
-struct Node* Create(int len){
-    struct Node* head = malloc(sizeof(struct Node*));
-    struct Node* tmp = head;
-    if(len == 0){
-        return NULL;
-    }
-    if(len == 1){
-        printf("enter value of index %d : ",i);
-        scanf("%d",&tmp->data);
-        tmp->next = NULL
-    }
-    for(int i = 0; i<len;i++){
-        printf("enter value of index %d : ",i);
-        scanf("%d",&tmp->data);
-        if(i == length - 1){
-            tmp->next = head;   
+int main(){
+    
+    // struct Node* first = (struct Node*)malloc(sizeof(struct Node*));
+    // struct Node* second = (struct Node*)malloc(sizeof(struct Node*));
+    // struct Node* third = (struct Node*)malloc(sizeof(struct Node*));
+    // struct Node* four = (struct Node*)malloc(sizeof(struct Node*));
+    // struct Node* Last = four;
+    // struct Node* Last.next = head;
+
+    // first->data = 1;
+    // second->data = 2;
+    // third->data = 3;
+    // four->data = 4;
+
+    // four->next = first
+    // first->next = second;
+    // second->next = third;
+    // third->next = four;
+    // four->next = Last;
+
+    struct Node* Last = (struct Node*)malloc(sizeof(struct Node*));
+    Last->next = (struct Node*)malloc(sizeof(struct Node*));
+    struct Node* tmp = Last->next;
+
+    for(int i = 0;i<3;i++){
+        
+        tmp->data = i;
+        if(i == 2){
+            tmp->next = Last;
         }
         else{
             tmp->next = (struct Node*)malloc(sizeof(struct Node*));
             tmp = tmp->next;
         }
     }
-    return tmp;
-}
-
-
-int main(){
-
+    tmp = Last->next;
+    for(int i = 0;i<3;i++){
+        printf("%d->",tmp->data);
+        tmp = tmp->next;
+    }
+    display(Last);
     return 0;
 }
