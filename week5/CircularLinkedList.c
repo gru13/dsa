c8fd84716a141f8f7c8e93b45d9bc4e1fed37611
Guruprasath <guruprasath1302@gmail.com>
2023-03-27 06:30:50 +0000

    creation and display cll

--- week5/CircularLinkedList.c
+++ week5/CircularLinkedList.c
@@ -15,34 +15,16 @@ void display(struct Node* last){
     printf("||");
 }
 
-int main(){
-    
-    // struct Node* first = (struct Node*)malloc(sizeof(struct Node*));
-    // struct Node* second = (struct Node*)malloc(sizeof(struct Node*));
-    // struct Node* third = (struct Node*)malloc(sizeof(struct Node*));
-    // struct Node* four = (struct Node*)malloc(sizeof(struct Node*));
-    // struct Node* Last = four;
-    // struct Node* Last.next = head;
-
-    // first->data = 1;
-    // second->data = 2;
-    // third->data = 3;
-    // four->data = 4;
-
-    // four->next = first
-    // first->next = second;
-    // second->next = third;
-    // third->next = four;
-    // four->next = Last;
-
-    struct Node* Last = (struct Node*)malloc(sizeof(struct Node*));
+struct Node* Create(struct Node* Last, int length){
+    if(Last == NULL){
+       struct Node* Last = (struct Node*)malloc(sizeof(struct Node*)); 
+    }
     Last->next = (struct Node*)malloc(sizeof(struct Node*));
     struct Node* tmp = Last->next;
-
-    for(int i = 0;i<3;i++){
-        
-        tmp->data = i;
-        if(i == 2){
+    for(int i = 0;i<length;i++){
+        printf("enter value of index %d : ",i);
+        scanf("%d",&tmp->data);
+        if(i == length - 1){
             tmp->next = Last;
         }
         else{
@@ -50,11 +32,16 @@ int main(){
             tmp = tmp->next;
         }
     }
-    tmp = Last->next;
-    for(int i = 0;i<3;i++){
-        printf("%d->",tmp->data);
-        tmp = tmp->next;
-    }
+    return Last;
+
+
+}
+int main(){
+    
+
+
+    struct Node* Last = (struct Node*)malloc(sizeof(struct Node*));
+    Last = Create(Last, 5);
     display(Last);
     return 0;
 }
