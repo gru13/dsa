c17c80946365f89028de807ba4c1d9d41e815279
gru13 <guruprasath1302@gmail.com>
2023-04-13 10:58:32 +0530

    .
    Signed-off-by: gru13 <guruprasath1302@gmail.com>

--- week6/stack_array.c
+++ week6/stack_array.c
@@ -44,6 +44,24 @@ int* pop(int* stack){
     }
     return stack;
 }
+int* pop(int* stack, int top){
+    if(top == -1){
+        printf("\nNO ELEMENT TO POP\n");
+    }
+    else{
+        int* nw  = (int*)malloc(sizeof(int*)*(top-1));
+        for(int i = 0;i<=top-1;i++){
+            nw[i] = stack[i+1];
+            // printf("\n%d\n",stack[i+1]);
+        }
+        stack = nw;
+        top--;
+        nw = NULL;
+    }
+    return stack;
+}
+
+
 
 void display(int* stack){
     int d = 0;
@@ -55,18 +73,48 @@ void display(int* stack){
     printf("\n");
 }
 
+int top_val(int* stack){
+    return stack[0];
+}
+
+int* reverse(int* stack){
+    int* rev =  (int*)malloc(sizeof(int)*(1+h));
+    int TOP = top;
+    top = 0;
+    while(top != -1){
+        int j = top_val(stack);
+        display(stack);
+        printf("%d\n",top);
+        rev = push(rev,j);
+        stack = pop(stack);
+        top--;
+        h++;
+    }
+    display(rev);
+    top = TOP;
+    stack = rev; 
+    return stack;
+}
+
 int main(){
     stack = push(stack,5);
     stack = push(stack,4);
     stack = push(stack,3);
     stack = push(stack,2);
     stack = push(stack,1);
-    display(stack);
-    stack = pop(stack);
-    display(stack);
-    stack = pop(stack);
-    display(stack);
-    stack = pop(stack);
+
+    // printf("%d\n", pop_val(stack));
+    // display(stack);
+    // stack = pop(stack);
+    // display(stack);
+    // stack = pop(stack);
+    // display(stack);
+    // stack = pop(stack);
+    // display(stack);
+    // stack = pop(stack);
+    // display(stack);
+    // printf("%d", top_val(stack));
+    stack = reverse(stack);
     display(stack);
     return 0;
 }
