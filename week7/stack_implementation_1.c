373b5b2b41f9c71dea7ecad521c698fe0db486ee
gru13 <guruprasath1302@gmail.com>
2023-04-23 15:13:31 +0530

    reverse stack implementation
    Signed-off-by: gru13 <guruprasath1302@gmail.com>

--- week7/stack_implementation_1.c
+++ week7/stack_implementation_1.c
@@ -3,105 +3,89 @@
 
 struct Stack{
     int top;
-    int* stack; 
+    char* stack;
 };
 
-struct charStack{
-    int top;
-    char* stack; 
-};
+struct Stack* create(){
+    struct Stack* S = (struct Stack*)malloc(sizeof(struct Stack*));
+    S->stack = NULL;
+    S->top = -1;
+    return S;
+}
+
+void display(struct Stack* stack){
+    printf("\n----------stack-----------\n");
+    printf("--------------------------\n");
+    for(int i =0;i<=stack->top;i++){
+        printf("%c->",stack->stack[i]);
+    }
+    printf("\n--------------------------\n");
+}
 
 
-
-
-struct charStack* push(struct charStack* Stack,int val){
-    Stack->top++;
-    char* nw = (char*)malloc(sizeof(int)*(1+Stack->top));
-    for(int i = 0;i <= Stack->top;i++){
-        if(i == 0){
-            nw[0]=val;
+char top(struct Stack* S){
+    if(S->top==-1){return '\0';}
+    return S->stack[0];
+}
+struct Stack* push(struct Stack* stack, char val){
+    if(stack->top==-1){
+        stack->top++;
+        stack->stack = (char*)malloc(sizeof(char)*stack->top+1);
+        stack->stack[0] = val;
+    } else {
+        stack->top++;
+        char* k = (char*)malloc(sizeof(char)*stack->top+1);
+        for(int i = 1;i<=stack->top;i++){
+            k[i] = stack->stack[i-1];
         }
-        else{
-            nw[i] = Stack->stack[i-1];
+        k[0] = val;
+        stack->stack = k;
+    }
+    return stack;
+}
+
+
+struct Stack* pop(struct Stack* S){
+    if(S->top == -1){
+        return NULL;
+    }
+    if(S->top == 0){
+        S->top--;
+        S->stack = NULL;
+    } else {
+        S->top--;
+        char* k = (char*)malloc(sizeof(char)*S->top+1);
+        for(int i = 0;i<=S->top;i++){
+            k[i] = S->stack[i+1];
         }
+        S->stack = k;
     }
-    Stack->stack = nw;
-    nw = NULL;
-
-    return Stack;
+    return S;
 }
 
 
-struct charStack* pop(struct charStack* Stack){
-    if(Stack->top == -1){
-        printf("\nNO ELEMENT TO POP\n");
+
+struct Stack* reverse(struct Stack* S){
+    struct Stack* rev = create();
+    while(S->top>=0){
+        char val = top(S);
+        rev = push(rev,val);
+        S = pop(S);
     }
-    else{
-        char* nw  = (char*)malloc(sizeof(char*)*(Stack->top-1));
-        for(int i = 0;i <= Stack->top-1;i++){
-            nw[i] = Stack->stack[i+1];
-            // printf("\n%d\n",stack[i+1]);
-        }
-        Stack->stack = nw;
-        Stack->top--;
-        nw = NULL;
-    }
-    return Stack;
+    free(S);
+    return rev;
 }
 
-
-void display(struct charStack* Stack){
-    printf("\n----------------------------------------------------------\n");
-    printf("--------------------------stack---------------------------\n");
-    printf("----------------------------------------------------------\n\n");
-    if(Stack->top == -1){
-        printf("No Element in stack\n");
-    }
-    for(int i = 0;i<=Stack->top;i++){
-        printf("%c\n",Stack->stack[i]);
-    }
-    printf("\n----------------------------------------------------------\n");
-    printf("----------------------------------------------------------\n\n");
-}
-
-char top_val(struct charStack* Stack){
-    return Stack->stack[0];
-}
-
-
-int top_int(struct charStack* Stack){
-    int val = 0;
-    int i = 0;
-    while(i <= Stack->top && Stack->stack[i]>=48 && Stack->stack[i]<=57){
-        val *= 10;
-        val += Stack->stack[i]-48;
-        i++;
-    }
-    return val;
-}
-
-// struct charStack* reverse(struct charStack* Stack){
-//     struct charStack* rev;
-//     // display(stack);
-//     while(Stack->top != -1){
-//         rev = push(rev,top_val(Stack));
-//         Stack = pop(Stack);
-//         Stack->top--;
-//         rev->top++;
-//     }
-//     return rev;
-// }
 
 int main(){
-    struct charStack* stack = (struct charStack*)malloc(sizeof(struct charStack*));
-    stack->stack = NULL;
-    stack->top = -1;
-    printf("-------------------------------");               
-
-    stack = push(stack,'1');
-    stack = push(stack,'-');
-    stack = push(stack,'2');
-    stack = push(stack,'3');
-    display(stack);
-    // printf("\n%d\n",top_int(stack));
+    struct Stack* S = create();
+    S = push(S,'1');
+    S = push(S,'2');
+    S = push(S,'3');
+    S = push(S,'4');
+    display(S);
+    // printf("%c\n",top(S));
+    // S = pop(S);
+    S = reverse(S);
+    display(S);
 }
