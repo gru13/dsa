73db60f02cb5387fe0bb7bcd1a627cd4f0b81e68
Guruprasath <guruprasath1302@gmail.com>
2023-05-15 05:18:46 +0000

    .

--- week10/tree.c
+++ week10/tree.c
@@ -2,8 +2,7 @@
 #include<stdlib.h>
 #include<limits.h>
 
-struct Node
-{
+struct Node{
     struct Node* lft;
     struct Node* ryt;
     int data;
@@ -24,7 +23,7 @@ void inorder(struct Node* node){
     inorder(node->lft);
  
     /* then print the data of node */
-    return node->data;
+    printf("%d ", node->data);
  
     /* now recur on right child */
     inorder(node->ryt);
@@ -58,17 +57,32 @@ void postorder(struct Node* node){
 }
 
 void insert(struct Node* tree, int val){
-    if(val<tree->data)
+    if(val < tree->data){
+        if(tree->lft == NULL){
+            tree->lft = create_node(val);
+            return;
+        }else{
+            return insert(tree->lft,val);
+        }
+    }
+    if(val > tree->data){
+        if(tree->ryt == NULL){
+            tree->ryt = create_node(val);
+            return;
+        }else{
+            return insert(tree->ryt,val);
+        }
+    }
 }
 
 int main(){
-    struct Node* Root = create_node(1);
-    struct Node* l1 = create_node(2);
-    struct Node* l2 = create_node(3);
-    struct Node* l1l = create_node(4);
-    struct Node* l2l = create_node(6);
-    struct Node* l1r = create_node(5);
-    struct Node* l2r = create_node(7);
+    struct Node* Root = create_node(8);
+    struct Node* l1 = create_node(3);
+    struct Node* l2 = create_node(10);
+    struct Node* l1l = create_node(2);
+    struct Node* l1r = create_node(4);
+    struct Node* l2l = create_node(9);
+    struct Node* l2r = create_node(11);
     Root->lft = l1;
     l1->lft = l1l;
     l1->ryt = l1r;
@@ -77,8 +91,12 @@ int main(){
     l2->ryt = l2r;
     inorder(Root);
     printf("\n");
-    preorder(Root);
+    // preorder(Root);
+    // printf("\n");
+    // postorder(Root);
+    // printf("\n");
+    insert(Root,55);
+    inorder(Root);
     printf("\n");
-    postorder(Root);
 
 }
